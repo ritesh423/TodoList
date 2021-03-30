fcea08398b784a69364a3c56c65b3869ce4b0bb4
ritesh <riteshsingh.py@gmail.com>
2021-03-30 12:20:49 +0530

    Maded the work and about us page

--- app.js
+++ app.js
@@ -1,50 +1,54 @@
+// jshint esversion:6
+
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname+"/date.js");
 
 const app = express();
-
-let items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    let today = new Date();
-
-    let options = {
-        weekday: "long",
-        year: "numeric",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("en-US", options)
-
-    res.render("list", {listTitle: day, newListItems: items});
+//GET function for home route
+app.get ("/", function(req,res){
+const day = date.getDate(); 
+  res.render("list", {listTitle:day, newListItems: items});
 
 });
+//POST function for home route
+app.post("/", function(req, res){
+const item = req.body.newItem;
 
-app.post("/", (req,res) => {
-    let item = req.body.newItem;
-
-    if (req.body.list === "Work") {
-        workItems.push(items);
-        res.redirect("/work");
-    } else {
-        items.push(item);
-        res.redirect("/")
-    }
-    items.push(item)
-    res.redirect("/");
-
+  if(req.body.list === "Work list"){
+    workItems.push(item);
+    res.redirect ("/work");
+  }else{
+    items.push(item);
+    res.redirect ("/");
+  }
+});
+//GET function for Work route
+app.get("/work", function(req,res){
+  res.render("list", {listTitle:"Work list", newListItems:workItems});
 });
 
-app.get("/work", (req,res) => {
-    res.render("list", {listTitle: "Work List", newListItems: workItems});
+//POST function for home route
+app.post("/work", function(req,res){
+  const item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+//GET function for About route
+
+app.get("/about", function(req,res){
+  res.render("about");
+});
+
+
+app.listen(3000, function(){
+console.log("Server is running on port 3000");
 });
